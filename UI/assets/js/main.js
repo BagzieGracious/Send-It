5a4004c71ac49b60043c9a25a8e43f408aa291bc
BagzieGracious <bagendadeogracious@gmail.com>
2018-11-05 19:43:52 +0300

    add assets folder with css, js, and images

---
+++ UI/assets/js/main.js
@@ -0,0 +1,74 @@
+var lgn = document.getElementById("login-wrapper");
+var sgn = document.getElementById("signup-wrapper");
+lgn.style.display = "none";
+sgn.style.display = "none";
+
+function login(){
+    lgn.style.display = "block";
+}
+
+function signin(){
+    var username = document.getElementById("login-username");
+    var password = document.getElementById("login-password");
+    var error = document.getElementById("login-error");
+
+    if( username.value == '' || password.value == ''){
+        error.innerHTML = "<strong>Error! </strong> no empty field is allowed";
+        error.style.display = "block";
+
+        setTimeout(function(){
+            error.style.display = "none";
+        }, 3000)
+    }else{
+        if (username.value == 'admin' && password.value == 'admin'){
+            window.location.href = "pages/admin/orders.html";
+        }else{
+            window.location.href = "pages/user/order.html";
+        }
+    }
+}
+
+function signup(){
+    sgn.style.display = "block";
+}
+
+function adduser(){
+    var usr = document.getElementById("sign-username");
+    var ful = document.getElementById("sign-fullname");
+    var eml = document.getElementById("sign-email");
+    var pas = document.getElementById("sign-password");
+    var error = document.getElementById("sign-error");
+
+    if(usr.value == '' && ful.value == '' && eml.value == '' && pas.value == ''){
+        error.innerHTML = "<strong>Error! </strong> No empty field is allowed";
+        error.style.display = "block";
+
+        setTimeout(function(){
+            error.style.display = "none";
+        }, 3000)
+    }else{
+        error.innerHTML = "<strong>Info! </strong> Thank you for registering.";
+        error.style.background = "#16a085";
+        error.style.display = "block";
+
+        setTimeout(function(){
+            error.style.display = "none";
+        }, 3000)
+    }
+}
+
+function changedesination(){
+    lgn.style.display = "block";
+}
+
+function changed(){
+    var dest = document.getElementById("destination");
+    var area = document.getElementById("area")
+    area.innerHTML = dest.value;
+    lgn.style.display = "none";
+}
+
+function cls(){
+    lgn.style.display = "none";
+    sgn.style.display = "none";
+}
